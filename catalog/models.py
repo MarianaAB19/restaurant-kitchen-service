f4b62747a27c875058b106dd81096305799b1524
Mariana <mariana.antoniak.babish@gmail.com>
2025-07-28 22:43:16 +0300

    add Dish model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -30,3 +31,18 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return f"{self.name} - {self.price} ₴"
