3439a3fee9fc52cdbf123a2f0cff54f6820e57f6
Mariana <mariana.antoniak.babish@gmail.com>
2025-08-07 20:31:33 +0300

    add Cook model to Admin Panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from catalog.models import DishType, Ingredient, Dish
+from django.contrib.auth.admin import UserAdmin
+
+from catalog.models import DishType, Ingredient, Dish, Cook
 
 
 @admin.register(DishType)
@@ -18,3 +20,25 @@ class DishAdmin(admin.ModelAdmin):
     list_display = ["name", "price", "dish_type", ]
     list_filter = ["dish_type", ]
     filter_horizontal = ["cooks", "ingredients", ]
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience", )
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience", )}), )
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
