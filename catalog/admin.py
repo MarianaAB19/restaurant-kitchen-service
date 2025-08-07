4dbe17ba5df15c15e3cf0d0e634e4e6608896f11
Mariana <mariana.antoniak.babish@gmail.com>
2025-08-07 14:47:39 +0300

    add Ingredient model to Admin Panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from catalog.models import DishType
+from catalog.models import DishType, Ingredient
 
 
 @admin.register(DishType)
 class DishTypeAdmin(admin.ModelAdmin):
+    search_fields = ["name", ]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
     search_fields = ["name", ]
